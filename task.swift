7c6d246703ab6eb2632aef09d66c1b0732134703
Kirill Kopylov <86028932+kkapi@users.noreply.github.com>
2024-02-19 13:21:42 +0300

    Create task.swift

---
+++ task.swift
@@ -0,0 +1,11 @@
+let name = "Кирилл"
+let surname = "Копылов"
+let group = "0371"
+let age = 21
+let height = 182.5
+
+print("Имя: ", name)
+print("Фамилия: ", surname)
+print("Группа: ", group)
+print("Возраст: ", age)
+print("Рост: ", height)
